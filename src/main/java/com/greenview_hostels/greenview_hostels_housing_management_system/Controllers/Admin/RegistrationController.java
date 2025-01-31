dc86e0be695a4ff3afba0ad12a8b32889b52822f
steve 4096 <smuturi795@gmail.com>
2025-01-31 22:40:38 +0300

    updates

---
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Admin/RegistrationController.java
@@ -0,0 +1,16 @@
+package com.greenview_hostels.greenview_hostels_housing_management_system.Controllers.Admin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class RegistrationController {
+    public Button Back_btn;
+    public TextField TenantID_txtfield;
+    public TextField Fname_txtfield;
+    public TextField Lname_txtfield;
+    public TextField Phonenumber_txtfield;
+    public TextField Email_txtfield;
+    public Label Email_errorlbl;
+    public Button Save_btn;
+}
