986b7d0a017bfa5d8975b1c8e713f7e8d4413445
steve 4096 <smuturi795@gmail.com>
2025-02-03 18:20:44 +0300

    Also added a listener to the password reset window that validates the password entered

--- src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Tenant/ForgotPasswordController.java
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Tenant/ForgotPasswordController.java
@@ -1,5 +1,7 @@
 package com.greenview_hostels.greenview_hostels_housing_management_system.Controllers.Tenant;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -18,15 +20,47 @@ public class ForgotPasswordController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        ResetPwd_btn.setOnAction(actionEvent -> checkIfPasswordsMatch());
+        ValidatePassword();
     }
 
     private void checkIfPasswordsMatch(){
         String password=NewPwd_txtarea.getText();
         String newPwd=ConfirmPwd_txtarea.getText();
-        if(password!=newPwd){
+        if(password.equals(newPwd)){
+            Pwd_dont_match_errorlbl.setText("");
+        }else {
             Pwd_dont_match_errorlbl.setText("Passwords don't match");
         }
+        System.out.println(password);
+        System.out.println(newPwd);
+    }
+
+    private void ValidatePassword(){
+        NewPwd_txtarea.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue,String oldValue, String newValue) {
+                StringBuilder errorMessage=new StringBuilder();
+                //Check for atleast 1 uppercase letter
+                if(!newValue.matches(".*[A-Z].*")){
+                    errorMessage.append("Password must contain atleast 1 uppercase letter.");
+                }
+                //Check for special characters
+                if(!newValue.matches(".*[^a-zA-Z0-9].*")){
+                    errorMessage.append("Password must contain atleast 1 special character");
+                }
+                //Check for number
+                if (!newValue.matches(".*\\d.*")){
+                    errorMessage.append("Password must contain atleast 1 digit");
+                }
+                //Print the output to the PwdFormat_error label
+                if(errorMessage.length()>0){
+                    Pwdformat_errorlbl.setText(errorMessage.toString().trim());
+                }else {
+                    Pwdformat_errorlbl.setText("");
+                }
+            }
+        });
     }
 
 
