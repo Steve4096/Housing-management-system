7f0d6139e7f322be343eaa61390422eaebf14925
steve 4096 <smuturi795@gmail.com>
2025-02-04 21:10:54 +0300

    I have made the various aspects that will form the AdminDashboard UI(the top bar,center pane and the admin menu)

---
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Admin/AdminController.java
@@ -0,0 +1,4 @@
+package com.greenview_hostels.greenview_hostels_housing_management_system.Controllers.Admin;
+
+public class AdminController {
+}
--- src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Admin/AdminMenuController.java
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Admin/AdminMenuController.java
@@ -1,4 +1,15 @@
 package com.greenview_hostels.greenview_hostels_housing_management_system.Controllers.Admin;
 
+import javafx.scene.control.Button;
+
 public class AdminMenuController {
+    public Button Dashboard_btn;
+    public Button Payments_btn;
+    public Button Add_Tenant_btn;
+    public Button Tenants_btn;
+    public Button UtilityLevels_btn;
+    public Button Complaints_btn;
+    public Button Notices_btn;
+    public Button Houses_btn;
+    public Button LogOut_btn;
 }
