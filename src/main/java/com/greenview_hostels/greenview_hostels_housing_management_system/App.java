fcbe5272524675643391d6ac0a32b7abcd86903f
steve 4096 <smuturi795@gmail.com>
2025-02-18 19:04:12 +0300

    updates

--- src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/App.java
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/App.java
@@ -1,17 +1,34 @@
 package com.greenview_hostels.greenview_hostels_housing_management_system;
 
 import com.greenview_hostels.greenview_hostels_housing_management_system.Models.Model;
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
 public class App extends Application {
     @Override
     public void start(Stage stage)  {
-        Model.getInstance().getViewsfactory().showForgotPasswordScreen();
+        Stage splashStage=Model.getInstance().getViewsfactory().showSplashScreen();
+
+        PauseTransition delay=new PauseTransition(Duration.seconds(5));
+        delay.setOnFinished(actionEvent ->{
+            splashStage.close();
+            OpenLoginAccountSelector();
+                }
+        );
+        delay.play();
+
+
+
+    }
+
+    private void OpenLoginAccountSelector(){
+        Model.getInstance().getViewsfactory().showLoginAccountSelectorWindow();
     }
 
 
