9687b06cec7c95963bb5d6227718cc8b0e9f33d8
steve 4096 <smuturi795@gmail.com>
2025-02-20 23:31:55 +0300

    Added some new windows on the tenants side

---
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Tenant/TenantController.java
@@ -0,0 +1,16 @@
+package com.greenview_hostels.greenview_hostels_housing_management_system.Controllers.Tenant;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TenantController implements Initializable {
+    public BorderPane Tenant_parent;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
