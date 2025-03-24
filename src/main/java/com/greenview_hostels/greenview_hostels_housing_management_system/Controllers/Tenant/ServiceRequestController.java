7bdfb84e73da9d6e897a044ac538c46e91645c98
steve 4096 <smuturi795@gmail.com>
2025-03-24 20:34:46 +0300

    updates

--- src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Tenant/ServiceRequestController.java
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Tenant/ServiceRequestController.java
@@ -1,15 +1,36 @@
 package com.greenview_hostels.greenview_hostels_housing_management_system.Controllers.Tenant;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 
-public class ServiceRequestController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ServiceRequestController implements Initializable {
     public Label Name_lbl;
     public Label House_lbl;
     public ComboBox House_or_Store_selector;
     public ComboBox Request_nature_selector;
     public TextArea Request_description_txtarea;
     public Button Submit_btn;
+
+    ObservableList<String> options= FXCollections.observableArrayList(
+            "Complaint","Repair/Maintenance"
+    );
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Request_nature_selector.setItems(options);
+        Submit_btn.setOnAction(actionEvent -> resetAllFields());
+    }
+
+    private void resetAllFields(){
+        Request_nature_selector.setValue("");
+        Request_description_txtarea.setText("");
+    }
 }
