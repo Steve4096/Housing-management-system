cafc2eb5694d4aa2d3bb184c26916f22f3696fc1
steve 4096 <smuturi795@gmail.com>
2025-03-27 13:17:32 +0300

    updates

--- src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Tenant/LoginController.java
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Tenant/LoginController.java
@@ -1,6 +1,7 @@
 package com.greenview_hostels.greenview_hostels_housing_management_system.Controllers.Tenant;
 
 import com.greenview_hostels.greenview_hostels_housing_management_system.Models.Model;
+import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.Initializable;
@@ -21,9 +22,11 @@ public class LoginController implements Initializable {
     public Hyperlink Forgot_pwd_link;
     public Button Signin_btn;
     public Label Invalid_email_address_lbl;
+    public Label Error_lbl;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        setupLoginButton();
         Forgot_pwd_link.setOnAction(actionEvent -> RedirectToResetPasswordScreen());
         CheckEmailAddressFormat();
         Back_btn.setOnAction(actionEvent -> showLoginAccountSelector());
@@ -56,9 +59,36 @@ public class LoginController implements Initializable {
    }
 
    private void login(){
+        String email= Email_textarea.getText();
+        String password=Pwd_txtarea.getText();
+        Model.getInstance().evaluateClientCredentials(email,password);
+        if (Model.getInstance().getTenantLoginSuccessFlag()){
+            successfulLogin();
+        }else {
+            invalidCredentials();
+        }
+   }
+
+   private void successfulLogin(){
         Stage stage=(Stage) Signin_btn.getScene().getWindow();
         Model.getInstance().getViewsfactory().CloseWindow(stage);
         Model.getInstance().getViewsfactory().showTenantDashboard();
+   }
+
+   private void invalidCredentials(){
+        Error_lbl.setText("INCORRECT PASSWORD OR EMAIL ADDRESS");
+        resetAllFields();
+   }
+
+   private void resetAllFields(){
+        Email_textarea.setText("");
+        Pwd_txtarea.setText("");
+   }
+
+   private void setupLoginButton(){
+        Signin_btn.disableProperty().bind(Bindings.createBooleanBinding(()->
+                Email_textarea.getText().trim().isEmpty() || Pwd_txtarea.getText().trim().isEmpty(),
+                Email_textarea.textProperty(),Pwd_txtarea.textProperty()));
    }
 }
 
