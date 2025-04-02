02b07c932ad7518d399f5f46792fd33e30375a9c
steve 4096 <smuturi795@gmail.com>
2025-04-02 15:48:09 +0300

    Added a simulation to monitor water levels and its usage

---
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Controllers/Admin/WaterLevelController.java
@@ -0,0 +1,12 @@
+package com.greenview_hostels.greenview_hostels_housing_management_system.Controllers;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+public class WaterLevelController {
+    public ProgressBar WaterlevelIndicator;
+    public Label WaterLevel_lbl;
+    public Label LastRefillDate_lbl;
+    public Button Refill_btn;
+}
---
+++ src/main/java/com/greenview_hostels/greenview_hostels_housing_management_system/Models/WaterLevelMonitor.java
@@ -0,0 +1,46 @@
+package com.greenview_hostels.greenview_hostels_housing_management_system.Models;
+
+import javafx.scene.control.ProgressBar;
+
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class WaterLevelMonitor {
+    private int waterlevel=80;
+    private final Random random=new Random();
+    private final WaterLevelListener listener;
+    private final ScheduledExecutorService executorService= Executors.newScheduledThreadPool(1);
+
+    public interface WaterLevelListener{
+        void onWaterLevelUpdate(int level);
+    }
+
+    public WaterLevelMonitor(WaterLevelListener listener){
+        this.listener=listener;
+        startSimulation();
+    }
+
+    private void startSimulation(){
+        executorService.scheduleAtFixedRate(()->{
+            //Simulate water usage
+            int change=random.nextInt(6)+1;
+            waterlevel=Math.max(waterlevel-change,0); //Ensure it doesnt go below 0
+
+            //Simulate refill
+            if (waterlevel<20 && random.nextBoolean()){
+                waterlevel=100;
+                System.out.println("Tank refilled");
+            }
+
+            //Notify UI
+            listener.onWaterLevelUpdate(waterlevel);
+
+            //Stop if tank is empty
+            if(waterlevel==0){
+                System.out.println("Tank is empty.Urgently refill");
+            }
+        },0,5, TimeUnit.SECONDS); //Updates every 5 seconds
+    }
+}
